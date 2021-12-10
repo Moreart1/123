eddbb01fc980a31aa67d93fcfc80bd7f0a44afb9
Mark <92625606+Moreart1@users.noreply.github.com>
2021-12-10 22:42:36 +0300

    Add files via upload

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_1._1
+{
+    class DoubleLinkedList : ILinkedList
+    {
+        private Node head = new Node();
+        private Node tail = new Node();
+        public DoubleLinkedList()
+        {
+            head.NextNode = tail;
+            tail.PrevNode = head;
+        }
+
+
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            Node current = head.NextNode;
+            while (current != tail && current != node)
+            {
+                current = current.NextNode;
+            }
+            if (current != tail)
+            {
+                current.NextNode = new()
+                {
+                    Value = value,
+                    PrevNode = current,
+                    NextNode = current.NextNode
+                };
+            }
+            else
+            {
+                throw new ArgumentException("Такого элемента нет в списке.", nameof(node));
+            }
+        }
+
+        public void AddNode(int value)
+        {
+            Node newNode = new Node { Value = 12345 };
+            if (head == null)
+                head.NextNode = new Node();
+            else
+            {
+                tail.NextNode = newNode;
+                newNode.PrevNode = tail;
+                tail = newNode;
+            }
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            Node current = head.NextNode;
+            while (current != tail && current.Value != searchValue)
+            {
+                current = current.NextNode;
+            }
+            if (current != tail)
+            {
+                return current;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int GetCount()
+        {
+            int count = 0;
+            Node current = head.NextNode;
+            while (current != tail)
+            {
+                current = current.NextNode;
+                count++;
+            }
+            return count;
+
+        }
+
+        public void RemoveNode(int index)
+        {
+            int count = 0;
+            Node current = head.NextNode;
+            while (current != tail && count < index)
+            {
+                current = current.NextNode;
+                count++;
+            }
+            if (count == index)
+            {
+                RemoveNode(current);
+            }
+            else
+            {
+                throw new ArgumentException("Элемента с таким индексом нет в списке.", nameof(index));
+            }
+        }
+
+        public void RemoveNode(Node node)
+        {
+            Node next = node.NextNode;
+            Node prev = node.PrevNode;
+
+            next.PrevNode = prev;
+            prev.NextNode = next;
+        }
+    }
+}
